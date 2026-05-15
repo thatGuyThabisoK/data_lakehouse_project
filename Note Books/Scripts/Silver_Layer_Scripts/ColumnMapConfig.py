5155f3aa1503d2a38a19aa4f6504158e95847c9e
thatGuyThabisoK <kobethabiso360@gmail.com>
2026-05-15 14:47:52 +0000

    changing column name
    mapping column names across the whole bronze layer using columnmapconfig file mappings

--- Note Books/Scripts/Silver_Layer_Scripts/ColumnMapConfig.py
+++ Note Books/Scripts/Silver_Layer_Scripts/ColumnMapConfig.py
@@ -1,48 +1,48 @@
 #Mappings for column name changes
 
-Location_table = {
-    "CID":"Customer id",
+customer_location = {
+    "CID":"Customer_id",
     "CNTRY":"Country"
 }
 
-Customer_demographic_info_table = {
-    "CID":"Customer id",
-    "B DATE":"Birth Date",
+customer_demographic_info = {
+    "CID":"Customer_id",
+    "B DATE":"Birth_Date",
     "GEN":"Gender"
 }
 
-Customer_info_table = {
-    "cst_id":"Customer id",
-    "cst_key":"Customer key",
-    "cst_firstname":"First name",
-    "cst_lastname":"Last name",
-    "cst_marital_status":"Marital status",
+customer_information = {
+    "cst_id":"Customer_id",
+    "cst_key":"Customer_key",
+    "cst_firstname":"First_name",
+    "cst_lastname":"Last_name",
+    "cst_marital_status":"Marital_status",
     "cst_gndr":"Gender"
 }
 
-Product_table = {
-    "prd_id":"Product id",
-    "prd_key":"Product key",
+product_information = {
+    "prd_id":"Product_id",
+    "prd_key":"Product_key",
     "prd_nm":"Name",
     "prd_cost":"Cost",
-    "prd_line":"Product line",
-    "prd_start_dt":"Start date",
-    "prd_end_dt":"End date"
+    "prd_line":"Product_line",
+    "prd_start_dt":"Start_date",
+    "prd_end_dt":"End_date"
 }
 
-Product_catagories_table = {
+product_catagories = {
     "CAT":"Category",
     "SUBCAT":"Subcategory",
     "MAINTENANCE":"Maintenance"
 }
 
-Sales_details_table = {
-    "sls_ord_num":"Order number",
-    "sls_prd_key":"Product key",
-    "sale_cust_id":"Customer id",
-    "sls_order_dt":"Order date",
-    "sls_ship_dt":"Ship date",
-    "sls_due_dt":"Due date",
+sales_details = {
+    "sls_ord_num":"Order_number",
+    "sls_prd_key":"Product_key",
+    "sale_cust_id":"Customer_id",
+    "sls_order_dt":"Order_date",
+    "sls_ship_dt":"Ship_date",
+    "sls_due_dt":"Due_date",
     "sls_sales":"Sales",
     "sls_quantity":"Quantity",
     "sls_price":"Price"
@@ -50,7 +50,7 @@ Sales_details_table = {
 
 #Table Names in the Silver Layer
 
-Table_names={
+table_names={
     "cust_az12_csv":"Customer_demographic_info",
     "cust_info_csv":"Customer_information",
     "loc_a101_csv":"Customer_Location",
